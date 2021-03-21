b3bd7af497d110b21929dcb4b2a7ac7a4b80716f
yu-shi1018 <yushisuwashita@gmail.com>
2021-03-21 04:55:22 +0000

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_many :post_images, dependent: :destroy
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '77152d9a0220ce180788532ffa106994a5a5c8c0c1c55ca32a37fd7c65172379ed56b0f58688c2e6b3d3fcb673a78c3177e667266ff074ca4ca3e29ea92ed511'
